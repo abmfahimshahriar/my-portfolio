6ade5f7e4f0bc1c4a1dae99ede9d1e440fc6dbfc
abmfahimshahriar <abmfahimshahriar@iut-dhaka.edu>
2021-04-21 14:03:54 +0600

    short description implementation

--- src/components/ShortDescription/ShortDescription.tsx
+++ src/components/ShortDescription/ShortDescription.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import "./ShortDescription.css";
-
+import my_animated_img from "../../Utilities/Images/my_animated_image.png";
 const ShortDescription = () => {
     return (
         <div className="short-desc-wrapper">
             <h1>Full Stack Web Developer</h1>
             <p>I code and develop web applications and I love what I do.</p>
+
+            <div>
+                <img className="my-animated-img" src={my_animated_img} alt="my_animated"/>
+            </div>
         </div>
     )
 }
