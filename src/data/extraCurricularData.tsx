fb0ffd87dc3ff884fa878286a4624b497cf52593
abmfahimshahriar <abmfahimshahriar@iut-dhaka.edu>
2021-05-11 04:34:34 +0200

    spelling error fix

--- src/data/extraCurricularData.tsx
+++ src/data/extraCurricularData.tsx
@@ -1,6 +1,6 @@
 export const extraCurricularData = [
     `Worked as the Treasurer in IUT Computer Society(IUTCS) 
-    and organized IUT 10thICT Fest 2019.`,
+    and organized IUT 10th ICT Fest 2019.`,
     `Worked as the Assistant Treasurer in IUT Computer Society(IUTCS).`,
     `Worked as a volunteer in IUT 9th ICT Fest and IUT 8th ICT Fest.`,
     `Organized Farewell Program.`,
